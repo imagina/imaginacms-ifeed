0513e6683b57431737ba33b20b6bd3f239f00e22
Omar Chunza <odchunzag.une@gmail.com>
2023-09-27 12:37:22 -0500

    Actualizacion de modulo Ifeed a laravel 10.

--- Database/Migrations/2022_05_25_165712478152_create_ifeed_feeds_table.php
+++ Database/Migrations/2022_05_25_165712478152_create_ifeed_feeds_table.php
@@ -1,16 +1,14 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateIfeedFeedsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('ifeed__feeds', function (Blueprint $table) {
             $table->engine = 'InnoDB';
@@ -25,11 +23,9 @@ class CreateIfeedFeedsTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('ifeed__feeds');
     }
-}
+};
